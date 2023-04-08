b9c1798a183a434f3b46e222e9ae7456194e7b68
David-S-Florez-C <davidsflorezc.dev@gmail.com>
2023-04-07 22:22:19 -0500

    First commit of the 'back-end' branch - Added project

---
+++ spring-drivex-project/src/test/java/com/drivex/springdrivexproject/SpringDrivexProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivex.springdrivexproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDrivexProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
