c318867c01af8bdd6c255abb24a9bc4914784b14
David-S-Florez-C <davidsflorezc.dev@gmail.com>
2023-04-08 22:25:43 -0500

    Fifth commit - Added 'spring-drivex-project' folder

---
+++ spring-drivex-project/src/main/java/com/drivex/springdrivexproject/SpringDrivexProjectApplication.java
@@ -0,0 +1,13 @@
+package com.drivex.springdrivexproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDrivexProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDrivexProjectApplication.class, args);
+	}
+
+}
