56d7b31fca38b9a20cc7ba4f767dc8f74f521f85
David-S-Florez-C <davidsflorezc.dev@gmail.com>
2023-04-30 23:24:07 -0500

    CORS configuration added

--- spring-drivex-project/src/main/java/com/drivex/springdrivexproject/SpringDrivexProjectApplication.java
+++ spring-drivex-project/src/main/java/com/drivex/springdrivexproject/SpringDrivexProjectApplication.java
@@ -2,12 +2,27 @@ package com.drivex.springdrivexproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringDrivexProjectApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(SpringDrivexProjectApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer()
+	{
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
--- spring-drivex-project/src/main/java/com/drivex/springdrivexproject/security/AdapterSecurity.java
+++ spring-drivex-project/src/main/java/com/drivex/springdrivexproject/security/AdapterSecurity.java
@@ -5,16 +5,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
+@RestController
 public class AdapterSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
-        http.exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
-        http.oauth2Login();
-        http.logout().logoutSuccessUrl("/").permitAll();
+        http.authorizeRequests(a -> a
+                .antMatchers("/api/Admin/**","/api/Car/**", "/api/Client/**" ,"/api/Gama/**",
+                        "/api/Message/**", "/api/Reservation/**", "/api/Score/**").permitAll()
+                .anyRequest().authenticated()
+        ).exceptionHandling(e -> e
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+        ).oauth2Login();
+
         http.cors().and().csrf().disable();
     }
 }
